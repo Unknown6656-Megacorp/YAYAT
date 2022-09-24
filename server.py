35ec6c17e390cb78caada149afd2706489c819a5
unknown6656 <unknown84@gmx.de>
2022-09-24 19:32:13 +0200

    added various helper functions

--- server.py
+++ server.py
@@ -1,18 +1,32 @@
-from flask import Flask, request, send_from_directory
-import argparse
-import sys
+from datetime import datetime
 import os
 import os.path as osp
+import argparse
+
+from flask import Flask, request, send_from_directory
 
 
 CURRENT_DIR = osp.dirname(__file__)
 STATIC_DIR = osp.join(CURRENT_DIR, 'static')
 USER_DIR = osp.join(CURRENT_DIR, 'userdata')
+PROJECTS_DIR = osp.join(USER_DIR, 'projects')
+_DEBUG_ = True
 
 os.chdir(CURRENT_DIR)
 
-if not osp.isdir(USER_DIR):
-    os.mkdir(USER_DIR)
+if not osp.isdir(USER_DIR): os.mkdir(USER_DIR)
+if not osp.isdir(PROJECTS_DIR): os.mkdir(PROJECTS_DIR)
+
+
+
+def parse_utc(utc : str) -> datetime:
+    return datetime.strptime(utc, "%Y-%m-%dT%H-%M-%S.%fZ")
+
+def print_utc(utc : datetime) -> str:
+    return utc.strftime('%Y-%m-%dT%H-%M-%S.%f')[:-3] + 'Z'
+
+def print_utcnow() -> str: return print_utc(datetime.utcnow())
+
 
 
 app = Flask(__name__)
@@ -25,7 +39,7 @@ import server_api
 parser = argparse.ArgumentParser()
 parser.add_argument('--port', '-p', type = str, default = '42420', help = 'The listening port.')
 parser.add_argument('--hostname', '-n', type = str, default = '0.0.0.0', help = 'The host name.')
-parser.add_argument('--debug', '-d', type = bool, default = True, help = 'Runs the server in debug mode.')
+parser.add_argument('--debug', '-d', type = bool, default = _DEBUG_, help = 'Runs the server in debug mode.')
 args = parser.parse_args()
 
 
