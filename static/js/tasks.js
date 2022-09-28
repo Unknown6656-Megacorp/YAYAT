137e12892ff8cbce1c7d59b7a0b95717a2906172
unknown6656 <unknown84@gmx.de>
2022-09-28 23:50:21 +0200

    added task overview

---
+++ static/js/tasks.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+let html = '';
+
+for (const task of tasks)
+{
+    const href = `/yayat/projects/${task.project}/tasks/${task.id}`;
+    html += `
+        <task-card>
+            <task-preview href="${href}"></task-preview>
+            <task-info>
+                <a href="${href}">
+                    <h2 data-task-id="${task.id}">${task.name}</h2>
+                </a>
+                <span class="small">
+                    Created by <b>${task.creator}</b> ${print_utc(task.created)} in 
+                    <a href="/yayat/projects/${task.project}/">
+                        <b>project #${task.project}</b>
+                    </a>.
+                    Last modified ${print_utc(task.modified)}.
+                </span>
+                <task-progress>
+                    <progress-bar>
+                        <progress-bar-segment class="completed" style="width: 40%"></progress-bar-segment>
+                        <progress-bar-segment class="not-started" style="width: 60%"></progress-bar-segment>
+                    </progress-bar>
+                    <b class="small">
+                        {} / ${task.frames} frame${task.frame != 1 ? 's' : ''} annotated
+                    </b>
+                </task-progress>
+            </task-info>
+            <task-actions>
+                <button class="primary open" href="${href}">Open</button>
+                <button>[ TODO ]</button>
+                <button>[ TODO ]</button>
+            </task-actions>
+        </task-card>
+    `;
+}
+
+$('task-list').html(html);
+$('#task-count').text(tasks.length);
+
+$('task-preview[href], task-actions button.open[href]').click(elem => window.location.href = $(elem.target).attr('href'));
