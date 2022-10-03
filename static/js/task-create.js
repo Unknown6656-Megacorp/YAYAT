6f02f41e3ec57f655fa3a040fbae8e2b18ac1bad
unknown6656 <unknown84@gmx.de>
2022-10-03 12:42:49 +0200

    slimmed down JSON load

--- static/js/task-create.js
+++ static/js/task-create.js
@@ -2,11 +2,11 @@
 
 
 $('#btn-create').click(() => query_api_sync(
-    `projects/${project.id}/tasks/create`,
+    `projects/${project_id}/tasks/create`,
     { name: $('#task-name').val() },
     data => {
-        window.location.href = `/yayat/projects/${project.id}/tasks/${data.id}/`
+        window.location.href = `/yayat/projects/${project_id}/tasks/${data.id}/`
     },
     error => show_modal_notice('An error occurred', error, [['Ok', () => { }]])
 ));
-$('#btn-cancel').click(() => window.location.href = `/yayat/projects/${project.id}/`);
+$('#btn-cancel').click(() => window.location.href = `/yayat/projects/${project_id}/`);
