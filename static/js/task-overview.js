db146c9c9bc1e613c78f0c16a4c91e31ffbc48ec
unknown6656 <unknown84@gmx.de>
2022-10-04 23:02:52 +0200

    added task overview logic

--- static/js/task-overview.js
+++ static/js/task-overview.js
@@ -7,7 +7,23 @@ $('#date-created').text(task.created);
 $('#date-modified').text(task.modified);
 
 
+let html = '';
 
+for (const frame of task.frames)
+    html += `
+        <frame-card data-frame-id="${frame.id}"
+                    data-source="${frame.original_image_source}"
+                    ${frame.deleted ? 'deleted' : ''}
+                    ${frame.explicit_annotations.length > 0 ? 'annotated' : ''}>
+            <frame-text data-frame-id="${frame.id}">#${frame.id}</frame-text>
+            <frame-image data-frame-id="${frame.id}" style="--image-preview: url('/api/img/${task.project}/${task.id}/${frame.id}/preview')">
+            </frame-image>
+        </frame-card>
+    `;
 
-$('#task-overview').click(() => window.location.href = `/yayat/projects/${project.id}/tasks/`);
+$('frame-list').html(html);
+$('frame-card').click(e => {
+    const frame_id = $(e.target).attr('data-frame-id');
 
+    window.location.href = `./annotate?frame=${frame_id}`
+});
